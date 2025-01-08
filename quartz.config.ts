b4f8296d9a08b6538f15d24f0eff8c2c2bdaf53a
Kenneth Lu <qkennethlu@gmail.com>
2025-01-07 21:36:15 -0500

    Quartz sync: Jan 7, 2025, 9:36 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Amberglen",
+    pageTitle: "🏡 Amberglen",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
